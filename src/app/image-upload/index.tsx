ff8f3e93fefb37172f73ae02980144187d0fa5ac
KosukeIsogai <kouiso@ritmo.co.jp>
2024-08-15 14:31:50 +0900

    refactor: add useRef hook to ImageUpload component
    - Added the useRef hook to the ImageUpload component in order to store the URL of the selected image file.
    - Updated the handleImageChange function to use the useRef hook to store the URL instead of using state.
    - Updated the onChange event handler for the file input to use the useRef hook to update the field value.
    
    Fixes #123

--- src/app/image-upload/index.tsx
+++ src/app/image-upload/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./style.module.scss";
 import Image from "next/image";
 import {
@@ -15,24 +15,37 @@ export const ImageUpload = <T extends FieldValues>({
   control,
   name,
 }: ImageUploadProps<T>) => {
-  const { field } = useController({
+  const {
+    field: { ref, ...rest },
+  } = useController({
     name,
     control,
   });
-  const [profileImage, setProfileImage] = React.useState<string | null>(null);
+
+  const imageUrlRef = useRef<string | null>(null);
+
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
-      setProfileImage(URL.createObjectURL(e.target.files[0]));
-      field.onChange(e); // Ensure the field value is updated
+      const url = URL.createObjectURL(e.target.files[0]);
+      imageUrlRef.current = url;
+      rest.onChange(e); // Ensure the field value is updated
     }
   };
+
   return (
     <div className={styles.field}>
       <label>プロフィール画像</label>
-      <input type="file" {...field} onChange={handleImageChange} />
-      {profileImage && (
+      <input
+        type="file"
+        {...rest}
+        ref={(e) => {
+          ref(e);
+        }}
+        onChange={handleImageChange}
+      />
+      {imageUrlRef.current && (
         <Image
-          src={profileImage}
+          src={imageUrlRef.current}
           alt="Profile Image"
           width={100}
           height={100}
